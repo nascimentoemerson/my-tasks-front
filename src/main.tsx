caf58fdde8ec78fc750193095062a81b8ee79be1
nascimentoemerson <contatovemarketing@gmail.com>
2023-04-24 20:34:08 -0300

    fet: remove componentes and pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import TaskManagerPage from './pages/TaskManager/taskManagerPage'
-import TaskListPage from './pages/TaskList/TaskListPage'
+import App from './App'
 
 
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <TaskManagerPage/>
-    <TaskListPage/>
+    <App/>
   </React.StrictMode>,
 )
