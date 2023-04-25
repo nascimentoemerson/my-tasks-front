a9bcebf3202a8e212ea4f0f43343ef8d2ad1dd62
nascimentoemerson <contatovemarketing@gmail.com>
2023-04-24 21:49:52 -0300

    feat: button teste

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,13 @@
+import { Button } from '@mui/material'
 import { Routes, Route, Navigate } from 'react-router-dom'
 
 export const AppRoutes = () => {
 
     return (
         <Routes>
-            <Route path='/home' element={<p>Home</p>} />
+            <Route path='/home' element={<Button>Teste</Button>} />
 
-            <Route path='*' element={<Navigate to="/home"/>} />
-        </Routes>
+                <Route path='*' element={<Navigate to="/home" />} />
+            </Routes>
     )
 }
